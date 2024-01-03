ebdeb7029bfb47598980d3abf7618b301024c3c8
Furkan Akyüz <TunderEye_00@outlook.com>
2024-01-03 22:50:33 +0300

    Create a seperate move function

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,11 @@ extends CharacterBody2D
 var direction : Vector2 = Vector2.ZERO
 
 func _process(delta):
+	
+	move()
+
+
+func move():
 	direction = Input.get_vector("ui_left", "ui_right","ui_up", "ui_down")
 	velocity = direction * speed
 	move_and_slide()
