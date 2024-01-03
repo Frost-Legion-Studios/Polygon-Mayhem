9c75d1aedf17db0b9f452c517c0468aae9c6739d
Furkan Akyüz <TunderEye_00@outlook.com>
2024-01-03 22:25:58 +0300

    Remove debug print velocity function

--- Player/player.gd
+++ Player/player.gd
@@ -8,5 +8,4 @@ var direction : Vector2 = Vector2.ZERO
 func _process(delta):
 	direction = Input.get_vector("ui_left", "ui_right","ui_up", "ui_down")
 	velocity = direction * speed
-	print(velocity)
 	move_and_slide()
