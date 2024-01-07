c58df9e73d8cefe463848fb554756f71d0a80518
Furkan Akyüz <TunderEye_00@outlook.com>
2024-01-07 20:50:01 +0300

    Make enemy follow player

--- enemies/enemy_triangle/enemy_triangle.gd
+++ enemies/enemy_triangle/enemy_triangle.gd
@@ -5,3 +5,12 @@ extends CharacterBody2D
 
 var player_position : Vector2
 var target_direction : Vector2
+
+
+func _process(delta):
+	player_position = Gamedata.player_position
+	target_direction = (player_position - global_position).normalized()
+	
+	velocity = target_direction * move_speed
+	
+	move_and_slide()
