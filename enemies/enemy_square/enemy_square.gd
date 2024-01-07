68fa3ac38f4cd84718c8614aecaf89863a2526c8
Tuğsan <tugsancitir@hotmail.com>
2024-01-07 20:50:32 +0300

    Implement a player following function for the square enemy

--- enemies/enemy_square/enemy_square.gd
+++ enemies/enemy_square/enemy_square.gd
@@ -4,3 +4,12 @@ extends CharacterBody2D
 
 var player_position : Vector2
 var target_direction : Vector2
+
+
+func _process(delta):
+	player_position = Gamedata.player_position
+	target_direction = (position - player_position).normalized()
+	
+	velocity = target_direction * move_speed 
+	
+	move_and_slide()
