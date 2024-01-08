3a3daee6f256b56c5bae4c1b91ada99e2553215a
Furkan Akyüz <TunderEye_00@outlook.com>
2024-01-08 13:43:35 +0300

    Align code with coding standards

--- enemies/enemy_triangle/enemy_triangle.gd
+++ enemies/enemy_triangle/enemy_triangle.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 @export var move_speed : int = 250
 
 var player_position : Vector2
@@ -9,10 +8,10 @@ var target_direction : Vector2
 
 func _process(delta):
 	player_position = Gamedata.player_position
-	target_direction = (player_position - global_position).normalized()
+	target_direction = (player_position - position).normalized()
 	
 	velocity = target_direction * move_speed
 	
-	look_at(player_position)
-	
 	move_and_slide()
+	
+	look_at(player_position)
