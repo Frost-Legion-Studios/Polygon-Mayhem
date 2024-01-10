84076e26f61a967ecf56b6b4d6fd20aff6f86af5
Tuğsan <tugsancitir@hotmail.com>
2024-01-11 00:37:00 +0300

    Implement state change on transition signal

--- enemies/enemy_triangle/triangle_state_machine.gd
+++ enemies/enemy_triangle/triangle_state_machine.gd
@@ -8,7 +8,7 @@ var states : Dictionary
 func _ready():
 	for child in get_children():
 		if child is State:
-			states[child.name] = child
+			states[child.name.to_lower()] = child
 			child.transitioned.connect(on_state_transition)
 	if initial_state:
 		initial_state.enter()
@@ -24,4 +24,15 @@ func _physics_process(delta):
 		current_state.physics_update(delta)
 
 func on_state_transition(state, new_state_name):
-	pass
+	if state != current_state:
+		return
+	
+	var new_state = states.get(new_state_name.lower())
+	if !new_state:
+		return
+	
+	if current_state:
+		current_state.exit()
+	
+	new_state.enter()
+	current_state = new_state
