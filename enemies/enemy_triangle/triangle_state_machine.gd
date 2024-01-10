535efc632566f50ac130e818d94a8505b7c9ad0f
Tuğsan <tugsancitir@hotmail.com>
2024-01-11 00:34:51 +0300

    Create an initial state variable that can be set through the editor

--- enemies/enemy_triangle/triangle_state_machine.gd
+++ enemies/enemy_triangle/triangle_state_machine.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var initial_state : State
+
 var current_state : State
 var states : Dictionary
 
@@ -7,6 +9,9 @@ func _ready():
 	for child in get_children():
 		if child is State:
 			states[child.name] = child
+	if initial_state:
+		initial_state.enter()
+		current_state = initial_state
 
 func _process(delta):
 	if current_state:
