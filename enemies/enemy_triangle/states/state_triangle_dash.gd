2ac9f05c61124c4219a93b4cf4e835934a85faa2
Tuğsan <tugsancitir@hotmail.com>
2024-01-12 01:07:58 +0300

    Implement the dashing mechanic in dash state

--- enemies/enemy_triangle/states/state_triangle_dash.gd
+++ enemies/enemy_triangle/states/state_triangle_dash.gd
@@ -10,3 +10,18 @@ var is_dashing : bool
 func enter():
 	is_dashing = false
 
+func update(_delta):
+	if is_dashing == false:
+		is_dashing = true
+		#Charge time
+		self_parent.velocity = Vector2.ZERO
+		await get_tree().create_timer(0.6).timeout
+		#Dash length
+		self_parent.velocity = (Vector2(cos(self_parent.rotation), sin(self_parent.rotation))) * dash_speed
+		await get_tree().create_timer(0.4).timeout
+		#Cooldown
+		self_parent.velocity = Vector2.ZERO
+		await get_tree().create_timer(0.8).timeout
+		transitioned.emit(self, "StateTriangleFollow")
+		
+	self_parent.move_and_slide()
