492de99694b998ee3034ed938b53cd8bd5f172a2
Tuğsan <tugsancitir@hotmail.com>
2024-01-11 23:47:19 +0300

    Make triangle look at player

--- enemies/enemy_triangle/states/state_triangle_follow.gd
+++ enemies/enemy_triangle/states/state_triangle_follow.gd
@@ -5,3 +5,8 @@ extends State
 @export var speed : int = 250
 var target_position : Vector2
 
+
+func update(_delta : float):
+	target_position = Gamedata.player_position
+	self_parent.look_at(target_position)
+	
