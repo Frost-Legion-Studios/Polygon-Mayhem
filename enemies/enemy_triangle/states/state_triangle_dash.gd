d95b1195579304b8eb6da94a7ef1c6fee7686356
Tuğsan <tugsancitir@hotmail.com>
2024-01-12 01:05:22 +0300

    Declare initial variables for dash state

--- enemies/enemy_triangle/states/state_triangle_dash.gd
+++ enemies/enemy_triangle/states/state_triangle_dash.gd
@@ -1,2 +1,9 @@
 class_name StateTriangleDash
 extends State
+
+@export var self_parent : CharacterBody2D
+@export var dash_speed : int = 1500
+
+var target_position : Vector2
+var is_dashing : bool
+
